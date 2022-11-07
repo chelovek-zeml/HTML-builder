adbb6dfec3bf498aa91c068988fa69869d640dd7
chelovek-zeml <stranger37561@gmail.com>
2022-11-07 16:45:54 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const pathOrigin = path.join(__dirname, 'files');
+const pathResult = path.join(__dirname, 'files-copy');
 
+
+fs.mkdir(pathResult, { recursive: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+});
+
+fs.readdir(pathOrigin, (err, filesArray) => {
+    if (err) {
+        return console.error(err);
+    }
+    for(let file of filesArray) {
+        fs.copyFile(path.join(pathOrigin, file), path.join(pathResult, file), (err) => {
+            if(err) {
+                return console.error(err);
+            }
+        });
+    }
+});
